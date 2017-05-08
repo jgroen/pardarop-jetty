dd25a26cfc37d817cf5b860b77cf81de81413447
jgroen <josh.groen@gmail.com>
2017-05-08 16:08:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ iperf3 -s -p 5202 -D
 #cd /opt/jetty
 #java -jar start.jar jetty.home=/opt/jetty
 
-java -jar mobJettyServer.jar ./
+java -jar mobJettyServer.jar ./ 8080
     
 while true; do
     sleep 300
