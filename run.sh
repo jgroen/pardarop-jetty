1cf0db5a63b4bfaf685006dc55ba7e0c92ca4f43
jgroen <josh.groen@gmail.com>
2017-05-10 14:30:15 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ iperf3 -s -p 5202 -D
 
 mkdir files/opt2
 cd files/opt2
-wget -r - np -nd http://52.37.177.82/opt2/
+wget -r -np -nd http://52.37.177.82/opt2/
 cd
 
 #Uncomment the next line for http version
